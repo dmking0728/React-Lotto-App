80601d0d247814550b7a1be6964f998a37ad6da5
daniel king <danielking@daniels-MacBook-Pro.local>
2019-07-07 14:06:39 -0400

    Updates

---
+++ precache-manifest.d087daf18ce07dfb61f152afb61716ee.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "604cd4ffe479c490cd381b31220a76ca",
+    "url": "/React-Lotto-App/index.html"
+  },
+  {
+    "revision": "1cff1407c768c04528ab",
+    "url": "/React-Lotto-App/static/css/main.b86a36b4.chunk.css"
+  },
+  {
+    "revision": "2790c44532533aa1f119",
+    "url": "/React-Lotto-App/static/js/2.2668f2aa.chunk.js"
+  },
+  {
+    "revision": "1cff1407c768c04528ab",
+    "url": "/React-Lotto-App/static/js/main.034af2d6.chunk.js"
+  },
+  {
+    "revision": "8b202b2313970c3a5aec",
+    "url": "/React-Lotto-App/static/js/runtime~main.447bad98.js"
+  }
+]);
